6b7947ffcd372f77fc6723b8281b1661d5afe13d
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-08-17 00:30:46 +0530

    Fetching Data From Server And Rendering Into Ejs File

--- controllers/toDoController.js
+++ controllers/toDoController.js
@@ -2,10 +2,25 @@
 
 // Request Handlers
 
+
+var data = [
+  {item: 'Optimize React Redux Project'},
+  {item: 'Buy cookies'},
+  {item: 'Pay Phone bill'},
+  {item: 'Organize trello board'},
+  {item: 'Call Yo Mama'},
+  {item: 'Read the JS book'},
+
+]
+
+
+
 module.exports = (app) => {
 
 app.get('/todo' , (req,res) => {
-  res.render('todo')
+  res.render('todo' , {
+    todos : data
+  })
 })
 
 app.post('/todo' , (req,res) => {
