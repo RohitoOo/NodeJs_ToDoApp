9be33c9f63edd73da4d84310e658be359599bd22
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-10-25 20:17:52 +0530

    NodeJs Core Modules

--- server.js
+++ server.js
@@ -1,11 +1,54 @@
 const http = require('http')
 
 const server = http.createServer( (req,res) => {
+
     console.log("Server Is Created And Running")
-    res.writeHead(200, {'Content-Type' : 'text/plain'})
-    res.end("Rohito")
+    // const myObj = {
+    //     name : "Rohito"
+    // }
+    // res.end(JSON.stringify(myObj))
 
     // Sending Data via res can be Optimized By Using Streams 
+
+
+    // Basic Route 
+
+
+    if (req.url === "/api") {
+        res.writeHead(200, {'Content-Type' : 'application/json'})
+        const myObj = {
+            name : "Rohito",
+            route: req.url
+        }
+        res.end(JSON.stringify(myObj))
+    }
+
+    if (req.url === "/err") {
+        res.writeHead(404, {'Content-Type' : 'application/json'})
+        const myObj = {
+            name : "Rohito",
+            route: req.url,
+            Status: "Error"
+        }
+        res.end(myObj.toString())
+    }
+
+    if (req.url === "/") {
+        res.writeHead(200, {'Content-Type' : 'application/json'})
+        const myObj = {
+            name : "Rohito",
+            route: req.url
+        }
+        res.end(JSON.stringify(myObj))
+    } else {
+     
+            res.writeHead(200, {'Content-Type' : 'application/json'})
+            const myObj = {
+                name : "Rohito",
+                route: req.url
+            }
+            res.end(JSON.stringify(myObj))
+    }
 });
 
 server.listen(3000, () => {
